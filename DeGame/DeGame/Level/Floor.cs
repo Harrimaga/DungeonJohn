a92aac13f2f42d250458f3d5b437057bad37772a
Jeroen <Jeroen@HIJZ13>
2017-12-05 13:16:12 +0100

    Stuff aan Floor en Room toegevoegd

--- DeGame/DeGame/Level/Floor.cs
+++ DeGame/DeGame/Level/Floor.cs
@@ -1,11 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
 
-    class Floor
+class Floor
+{
+    string[,] floorarray = new string[100, 100];
+
+    public void MergeArrays (string[,] roomarray)
     {
-
+        int a = 0, b = 0;
+        // algoritme dat kamers indeeld en offset waarde aan a en b geeft
+        for (int x = 0; x == 10; x++)
+            for (int y = 0; y == 10; y++)
+                floorarray[x + a, y + b] = roomarray[x, y];
     }
+
+    public void Draw(GameTime gameTime)
+    {
+        for (int x = 0; x < 100; x++)
+            for (int y = 0; y < 100; y++)
+            {
+                //switch (roomarray[x,y])
+                //{
+                //    case '?':
+                //        return "Blank";
+                //    case '.':
+                //        return "Normal";
+                //    case '!':
+                //        return "Rock";
+                //    case '+':
+                //        return "Wall";
+                //    case 'X':
+                //        return "End";
+                //    case 'S':
+                //        return "Start";
+                //    default:
+                //        return "Unkown";
+            }
+    }
+
+
+}
 
--- DeGame/DeGame/Level/Room.cs
+++ DeGame/DeGame/Level/Room.cs
@@ -1,163 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.IO;
 
-    class Room
+class Room : GameObject
     {
     public void LoadTiles(string path)
     {
         List<string> textLines = new List<string>();
         StreamReader fileReader = new StreamReader(path);
         string line = fileReader.ReadLine();
-        int width = line.Length;
+        string[,] roomarray = new string[10, 8];
+
+        //int CellWidth = 50;
+        //int CellHeight = 50;
+        //int levelwidth = line.Length * CellWidth;
+        //int levelheight = textLines.Count * CellHeight;
+
+
         while (line != null)
         {
             textLines.Add(line);
             line = fileReader.ReadLine();
         }
-        TileField tiles = new TileField(textLines.Count - 2, width, 1, "tiles");
 
-        Add(tiles);
-        tiles.CellWidth = 72;
-        tiles.CellHeight = 55;
-        for (int x = 0; x < width; ++x)
+        for (int x = 0; x < line.Length; ++x)
         {
-            for (int y = 0; y < textLines.Count - 2; ++y)
+            for (int y = 0; y < textLines.Count; ++y)
             {
-                Tile t = LoadTile(textLines[y][x], x, y);
-                tiles.Add(t, x, y);
+                roomarray[x, y] = AssignType(textLines[y][x], x, y);
             }
         }
     }
 
-    private Tile LoadTile(char tileType, int x, int y)
+    private string AssignType(char tileType, int x, int y)
     {
         switch (tileType)
         {
+            case '?':
+                return "Blank";
             case '.':
-                return new Tile();
-            case '-':
-                return LoadBasicTile("spr_platform", TileType.Platform);
+                return "Normal";
+            case '!':
+                return "Rock";
             case '+':
-                return LoadBasicTile("spr_platform_hot", TileType.Platform, true, false);
-            case '@':
-                return LoadBasicTile("spr_platform_ice", TileType.Platform, false, true);
+                return "Wall";
             case 'X':
-                return LoadEndTile(x, y);
-            case 'W':
-                return LoadWaterTile(x, y);
-            case '1':
-                return LoadStartTile(x, y);
-            case '#':
-                return LoadBasicTile("spr_wall", TileType.Normal);
-            case '^':
-                return LoadBasicTile("spr_wall_hot", TileType.Normal, true, false);
-            case '*':
-                return LoadBasicTile("spr_wall_ice", TileType.Normal, false, true);
-            case 'T':
-                return LoadTurtleTile(x, y);
-            case 'R':
-                return LoadRocketTile(x, y, true);
-            case 'r':
-                return LoadRocketTile(x, y, false);
+                return "End";
             case 'S':
-                return LoadSparkyTile(x, y);
-            case 'A':
-            case 'B':
-            case 'C':
-                return LoadFlameTile(x, y, tileType);
+                return "Start";
             default:
-                return new Tile("");
+                return "Unkown"; 
         }
     }
-
-    //private tile loadbasictile(string name, tiletype tiletype, bool hot = false, bool ice = false)
-    //{
-    //    tile t = new tile("tiles/" + name, tiletype);
-    //    t.hot = hot;
-    //    t.ice = ice;
-    //    return t;
-    //}
-
-    //private tile loadstarttile(int x, int y)
-    //{
-    //    tilefield tiles = find("tiles") as tilefield;
-    //    vector2 startposition = new vector2(((float)x + 0.5f) * tiles.cellwidth, (y + 1) * tiles.cellheight);
-    //    player player = new player(startposition);
-    //    add(player);
-    //    return new tile("", tiletype.background);
-    //}
-
-    //private tile loadflametile(int x, int y, char enemytype)
-    //{
-    //    gameobjectlist enemies = find("enemies") as gameobjectlist;
-    //    tilefield tiles = find("tiles") as tilefield;
-    //    gameobject enemy = null;
-    //    switch (enemytype)
-    //    {
-    //        case 'a': enemy = new unpredictableenemy(); break;
-    //        case 'b': enemy = new playerfollowingenemy(); break;
-    //        case 'c':
-    //        default: enemy = new patrollingenemy(); break;
-    //    }
-    //    enemy.position = new vector2(((float)x + 0.5f) * tiles.cellwidth, (y + 1) * tiles.cellheight);
-    //    enemies.add(enemy);
-    //    return new tile();
-    //}
-
-    //private tile loadturtletile(int x, int y)
-    //{
-    //    gameobjectlist enemies = find("enemies") as gameobjectlist;
-    //    tilefield tiles = find("tiles") as tilefield;
-    //    turtle enemy = new turtle();
-    //    enemy.position = new vector2(((float)x + 0.5f) * tiles.cellwidth, (y + 1) * tiles.cellheight + 25.0f);
-    //    enemies.add(enemy);
-    //    return new tile();
-    //}
-
-
-    //private tile loadsparkytile(int x, int y)
-    //{
-    //    gameobjectlist enemies = find("enemies") as gameobjectlist;
-    //    tilefield tiles = find("tiles") as tilefield;
-    //    sparky enemy = new sparky((y + 1) * tiles.cellheight - 100f);
-    //    enemy.position = new vector2(((float)x + 0.5f) * tiles.cellwidth, (y + 1) * tiles.cellheight - 100f);
-    //    enemies.add(enemy);
-    //    return new tile();
-    //}
-
-    //private tile loadrockettile(int x, int y, bool movetoleft)
-    //{
-    //    gameobjectlist enemies = find("enemies") as gameobjectlist;
-    //    tilefield tiles = find("tiles") as tilefield;
-    //    vector2 startposition = new vector2(((float)x + 0.5f) * tiles.cellwidth, (y + 1) * tiles.cellheight);
-    //    rocket enemy = new rocket(movetoleft, startposition);
-    //    enemies.add(enemy);
-    //    return new tile();
-    //}
-
-    //private tile loadendtile(int x, int y)
-    //{
-    //    tilefield tiles = find("tiles") as tilefield;
-    //    spritegameobject exitobj = new spritegameobject("sprites/spr_goal", 1, "exit");
-    //    exitobj.position = new vector2(x * tiles.cellwidth, (y + 1) * tiles.cellheight);
-    //    exitobj.origin = new vector2(0, exitobj.height);
-    //    add(exitobj);
-    //    return new tile();
-    //}
-
-    //private tile loadwatertile(int x, int y)
-    //{
-    //    gameobjectlist waterdrops = find("waterdrops") as gameobjectlist;
-    //    tilefield tiles = find("tiles") as tilefield;
-    //    waterdrop w = new risingdrop();
-    //    w.origin = w.center;
-    //    w.position = new vector2(x * tiles.cellwidth, y * tiles.cellheight - 10);
-    //    w.position += new vector2(tiles.cellwidth, tiles.cellheight) / 2;
-    //    waterdrops.add(w);
-    //    return new tile();
-    //}
 }
 
