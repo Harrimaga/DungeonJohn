8f9ed2c240b04990aad8bfbd7f23347635a11f5c
Jeroen <Jeroen@HIJZ13>
2017-12-06 12:44:45 +0100

    alkjteoihowqte

--- DeGame/DeGame/Level/Floor.cs
+++ DeGame/DeGame/Level/Floor.cs
@@ -11,10 +11,12 @@ public class Floor
     Room[,] floor;
     int maxRooms = 20;
     int minRooms = 15;
+    int a = 7, b = 7;
 
     public Floor()
     {
         floor = new Room[9, 9];
+        //TODO iets wat elke room aan een plekje in een floorarray linked met coordinaat [a,b]
         //hele simpele layout voor testen
         //floor[5, 5] = new StartRoom(new Vector2(5,5));
         //floor[6, 5] = new Room();
@@ -36,7 +38,7 @@ public class Floor
         {
             if (room != null)
             {
-                room.Draw(gameTime, spriteBatch);
+                room.Draw(gameTime, spriteBatch, a, b);
             }
         }
     }
--- DeGame/DeGame/Level/Room.cs
+++ DeGame/DeGame/Level/Room.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 
 public class Room : GameObjectList
 {
@@ -36,20 +38,37 @@ public class Room : GameObjectList
     {
         switch (tileType)
         {
-            case '?':
-                return "Unknown";
             case '.':
                 return "Background";
-            case '!':
-                return "Rock";
-            case '+':
-                return "Wall";
-            case 'X':
-                return "Exit";
-            case 'S':
-                return "Start";
+            //case '!':
+            //    return "Rock";
+            //case '+':
+            //    return "Wall";
+            //case 'X':
+            //    return "Exit";
+            //case 'S':
+            //    return "Start";
+            //case '?':
+            //    return "Unknown";
             default:
                 return "N/A";
         }
+    }
+
+    public void Draw(GameTime gameTime, SpriteBatch spriteBatch, int a, int b)
+    {
+        for (int x = 0; x < 10; x++)
+            for (int y = 0; y < 8; y++)
+            {
+                switch (roomarray[x, y])
+                {
+                    case "Background":
+                        spriteBatch.Draw((GameEnvironment.assetManager.GetSprite("Sprites/Standardtile.png")), new Vector2(x * 50 + a * 500, y * 50 + b * 400), Color.Blue);
+                        break;
+                    default:
+                        spriteBatch.Draw((GameEnvironment.assetManager.GetSprite("Sprites/Standardtile.png")), new Vector2(x * 50 + a * 500, y * 50 + b * 400), Color.Black);
+                        break;
+                }
+            }
     }
 }
