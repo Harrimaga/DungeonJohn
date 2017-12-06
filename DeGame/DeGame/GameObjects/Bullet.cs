785343f27c01dfa09ccd5ca44c2348e984198f47
Fabian <fabia@DESKTOP-7MVPKR9>
2017-12-06 11:17:09 +0100

    blabla

--- DeGame/DeGame/GameObjects/Bullet.cs
+++ DeGame/DeGame/GameObjects/Bullet.cs
@@ -10,14 +10,21 @@ class Bullet : SpriteGameObject
 {
     public Bullet(Vector2 Startposition, int Direction)
     {
+        this.position = Startposition;
         if (Direction == 0)
-            velocity.Y = 400;
+            velocity.Y = 10;
         else if (Direction == 1)
-            velocity.X = -400;
+            velocity.X = -10;
         else if (Direction == 2)
-            velocity.Y = -400;
+            velocity.Y = -10;
         else if (Direction == 3)
-            velocity.X = 400;
+            velocity.X = 10;
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+        position += velocity;
     }
 
     public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
