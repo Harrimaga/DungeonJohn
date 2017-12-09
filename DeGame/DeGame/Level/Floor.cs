4da86441d303413120544a474199f6307b7d0250
Jeroen <Jeroen@HIJZ13>
2017-12-09 15:16:59 +0100

    doorgeven door bools floor -> room

--- DeGame/DeGame/Level/Floor.cs
+++ DeGame/DeGame/Level/Floor.cs
@@ -51,20 +51,19 @@ public class Floor
                 {
                     if (x + 1 < 9 && floor[x + 1, y] != null)
                     {
-                        // door right
+                        floor[x, y].right = true;
                     }
-
                     if (x - 1 >= 0 && floor[x - 1, y] != null)
                     {
-                        //door left
+                        floor[x, y].left = true;
                     }
                     if (y + 1 < 9 &&  floor[x, y + 1] != null)
                     {
-                        //door down
+                        floor[x, y].down = true;
                     }
                     if (y - 1 >= 0 && floor[x, y - 1] != null)
                     {
-                        //door up
+                        floor[x, y].up = true;
                     }
                 }
             }
