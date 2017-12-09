7bc06409f546b65c121d933a197aca1f8220292d
Romeo Zeph <r.s.b.zeph@students.uu.nl>
2017-12-09 14:59:27 +0100

    DoorCheck

--- DeGame/DeGame/Level/Floor.cs
+++ DeGame/DeGame/Level/Floor.cs
@@ -26,12 +26,13 @@ public class Floor
         //floor[7, 5] = new EndRoom(new Vector2(7,5));
     }
 
+ 
     void FloorGenerator()
     {
         int maxRooms = 20;
         int minRooms = 15;
-        int x = random.Next(9);
-        int y = random.Next(9);
+        int x = 5;//random.Next(9);
+        int y = 5;//random.Next(9);
         //floor[x,y]
         //TODO iets wat elke room aan een plekje in een floorarray linked met coordinaat [a,b]
     }
@@ -39,6 +40,35 @@ public class Floor
     void NextFloor()
     {
         //TODO dus new floor maken (FloorGenerator aanroepen) en oude weg halen
+    }
+    void DoorCheck()
+    {
+        for (int x = 0; x < 9; x++)
+        {
+            for (int y = 0; y < 9; y++)
+            {
+                if (floor[x, y] != null)
+                {
+                    if (x + 1 < 9 && floor[x + 1, y] != null)
+                    {
+                        // door right
+                    }
+
+                    if (x - 1 > 0 && floor[x - 1, y] != null)
+                    {
+                        //door left
+                    }
+                    if (y + 1 < 9 &&  floor[x, y + 1] != null)
+                    {
+                        //door down
+                    }
+                    if (y - 1 > 0 && floor[x, y - 1] != null)
+                    {
+                        //door up
+                    }
+                }
+            }
+        }
     }
 
     public virtual void Update(GameTime gameTime)
@@ -50,7 +80,7 @@ public class Floor
                 room.Update(gameTime);
             }
         }
-        //TODO als nextFloor true is dan voor NextFloor() uit
+        //TODO als nextFloor true is voer dan NextFloor() uit
     }
 
     public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch)
@@ -58,7 +88,7 @@ public class Floor
         for (int a = 0; a < 9; a++)
             for (int b = 0; b < 9; b++)
                 if (floor[a, b] != null)                
-                    floor[a, b].Draw(gameTime, spriteBatch, a, b);   
+                    floor[a, b].Draw(gameTime, spriteBatch, a, b);
     }
 
 }
