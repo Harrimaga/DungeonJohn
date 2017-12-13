7413425a63feb17ac0dc7c74e657202a025127ce
Yoran den Heijer <yorandenheijer@gmail.com>
2017-12-13 11:58:25 +0100

    .

--- DeGame/DeGame/GameManagement/Camera.cs
+++ DeGame/DeGame/GameManagement/Camera.cs
@@ -7,7 +7,7 @@ public class Camera
 
     static public void CameraPlacement(GameTime gameTime, Player player, int roomwidth, int roomheight)
     {
-        CameraPosition.X = MathHelper.Clamp(player.position.X /*- GameEnvironment.graphics.PreferredBackBufferWidth / 2*/, 0, roomwidth/* - GameEnvironment.graphics.PreferredBackBufferWidth*/);
+        CameraPosition.X = MathHelper.Clamp(player.position.X - GameEnvironment.graphics.PreferredBackBufferWidth / 2, 0, roomwidth/* - GameEnvironment.graphics.PreferredBackBufferWidth*/);
         CameraPosition.Y = MathHelper.Clamp(player.position.Y /*- GameEnvironment.graphics.PreferredBackBufferHeight / 2*/, 0, roomheight/* - GameEnvironment.graphics.PreferredBackBufferHeight*/);
     }
 
