411ef522054b3e68dd82eb3d4bbc435b7ba4812a
Jeroen <Jeroen@HIJZ13>
2017-12-13 13:44:12 +0100

    mhghjg

--- DeGame/DeGame/Level/Room.cs
+++ DeGame/DeGame/Level/Room.cs
@@ -130,7 +130,6 @@ public class Room : GameObjectList
     }
     public void Draw(GameTime gameTime, SpriteBatch spriteBatch, int a, int b)
     {
-        enemies.Draw(gameTime, spriteBatch);
         for (int x = 0; x < 10; x++)
             for (int y = 0; y < 8; y++)
             {
