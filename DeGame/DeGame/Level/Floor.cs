c4834447d2589504eb96feba74de2cfc1e2af980
Tim <timvanbrederode@live.nl>
2017-12-13 14:53:19 +0100

    Merged JMANBRANCH

--- DeGame/DeGame/Level/Floor.cs
+++ DeGame/DeGame/Level/Floor.cs
@@ -12,7 +12,6 @@ public class Floor
     int[,] backupchoice;
     int maxRooms = 5, minRooms = 5, floorWidth = 9, floorHeight = 9, CurrentLevel = 1, CurrentRooms, b = 0, q;
     Random random = new Random();
-    int CurrentRooms = 0;
     public Room currentRoom;
     bool FloorGenerated = false;
 
