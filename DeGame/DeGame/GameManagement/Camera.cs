2e250a143e257636863739ef530edbf7f06edae9
Yoran den Heijer <yorandenheijer@gmail.com>
2017-12-13 16:02:50 +0100

    niks

--- DeGame/DeGame/GameManagement/Camera.cs
+++ DeGame/DeGame/GameManagement/Camera.cs
@@ -5,11 +5,7 @@ public class Camera
 {
     static public Vector2 CameraPosition = new Vector2(0, 0);
 
-    static public void CameraPlacement(GameTime gameTime, int roomwidth, int roomheight)
-    {
-        CameraPosition.X = MathHelper.Clamp(PlayingState.player.position.X - 800, 0, 500 - 800);
-        CameraPosition.Y = MathHelper.Clamp(PlayingState.player.position.Y - 480 / 2, 0, 400 - 480);
-    }
+
 
     static public void Reset()
     {
