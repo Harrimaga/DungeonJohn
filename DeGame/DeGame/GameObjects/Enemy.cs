338876fce6830c8194260d4f6ba474875efddd5d
Robert van den Brink <robertvdenbrink@gmail.com>
2017-12-25 14:43:15 +0100

    space

--- DeGame/DeGame/GameObjects/Enemy.cs
+++ DeGame/DeGame/GameObjects/Enemy.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
- using RoyT.AStar;
+using RoyT.AStar;
 
 public class Enemy : SpriteGameObject
 {
