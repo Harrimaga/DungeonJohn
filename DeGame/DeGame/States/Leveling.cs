9ee09e142f155e52d2d42b65d6eec4f2cab9c3b3
Romeo Zeph <r.s.b.zeph@students.uu.nl>
2017-12-26 21:03:35 +0100

    niks

--- DeGame/DeGame/States/Leveling.cs
+++ DeGame/DeGame/States/Leveling.cs
@@ -10,10 +10,14 @@ using System.Threading.Tasks;
 class Leveling : IGameObject
 {
     Vector2 BasisPosition;
+    Button attackB, healthB;
     bool picked = false;
     protected IGameObject playingState;
     public Leveling()
     {
+        playingState = GameEnvironment.gameStateManager.GetGameState("Playing");
+        attackB = new Button(new Vector2(240,600), "Attack","n",1);
+        healthB = new Button(new Vector2(420, 600), "Health", "n", 1);
     }
     public virtual void HandleInput(InputHelper inputHelper)
     {
@@ -21,10 +25,16 @@ class Leveling : IGameObject
     public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch)
     {
         playingState.Draw(gameTime, spriteBatch);
-        spriteBatch.Draw(GameEnvironment.assetManager.GetSprite("Sprites/pauze"), BasisPosition);
+        attackB.Draw(gameTime, spriteBatch);
+        healthB.Draw(gameTime, spriteBatch);
+        spriteBatch.DrawString(GameEnvironment.assetManager.GetFont("Sprites/SpelFont"), "Level-Up",BasisPosition + new Vector2(600,240), Color.Black);
+        //spriteBatch.Draw(GameEnvironment.assetManager.GetSprite("Sprites/pauze"), BasisPosition);
     }
     public virtual void Update(GameTime gameTime)
     {
+        BasisPosition = new Vector2(Camera.Position.X - (GameEnvironment.WindowSize.X / 2), Camera.Position.Y - (GameEnvironment.WindowSize.Y / 2));
+        attackB.Update(gameTime);
+        healthB.Update(gameTime);
         if (picked)
         {
             GameEnvironment.gameStateManager.SwitchTo("Playing");
