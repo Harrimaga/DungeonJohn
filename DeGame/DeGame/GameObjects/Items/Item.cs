e50c3ea12353d1fbee57a1137c9d616862b86593
Tim <timvanbrederode@live.nl>
2017-12-28 17:03:00 +0100

    Started Items, added interfaces for each type

---
+++ DeGame/DeGame/GameObjects/Items/Item.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+
+public class Item : SpriteGameObject
+{
+    Vector2 position;
+
+    public Item(Vector2 position, int layer = 0, string id = "item") : base("Sprites/OldRock", layer, id)
+    {
+        this.position = position;
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+    }
+
+    public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+    {
+    }
+}
+
