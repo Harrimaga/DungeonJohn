91f5c62fd1841b01d22ae7e6308c05f740803a6c
Tim <timvanbrederode@live.nl>
2017-12-28 16:07:53 +0100

    Added basic comments for camera and GameObject

--- DeGame/DeGame/GameManagement/GameObject.cs
+++ DeGame/DeGame/GameManagement/GameObject.cs
@@ -22,38 +22,62 @@ public abstract class GameObject : IGameObject
     {
     }
 
+    /// <summary>
+    /// Calls the update for the GameObject
+    /// </summary>
+    /// <param name="gameTime">Current GameTime</param>
     public virtual void Update(GameTime gameTime)
     {
        position += velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
     }
 
+    /// <summary>
+    /// Calls the draw for the GameObject
+    /// </summary>
+    /// <param name="gameTime">Current GameTime</param>
+    /// <param name="spriteBatch">The spritebatch</param>
     public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch)
     {
     }
 
+    /// <summary>
+    /// Gets or sets the parent
+    /// </summary>
     public virtual GameObject Parent
     {
         get { return parent; }
         set { parent = value; }
     }
 
+    /// <summary>
+    /// Gets or sets the layer
+    /// </summary>
     public virtual int Layer
     {
         get { return layer; }
         set { layer = value; }
     }
 
+    /// <summary>
+    /// Gets the ID
+    /// </summary>
     public string Id
     {
         get { return id; }
     }
 
+    /// <summary>
+    /// Gets or sets the position
+    /// </summary>
     public virtual Vector2 Position
     {
         get { return position; }
         set { position = value; }
     }
 
+    /// <summary>
+    /// Gets the global position
+    /// </summary>
     public virtual Vector2 GlobalPosition
     {
         get
@@ -69,6 +93,9 @@ public abstract class GameObject : IGameObject
         }
     }
 
+    /// <summary>
+    /// Calculates the boundingbox
+    /// </summary>
     public virtual Rectangle BoundingBox
     {
         get
@@ -77,6 +104,9 @@ public abstract class GameObject : IGameObject
         }
     }
 
+    /// <summary>
+    /// Reset the gameobject
+    /// </summary>
     public virtual void Reset()
     {
     }
