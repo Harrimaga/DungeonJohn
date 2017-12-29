351844b8686a7e971839635b043bca024054f165
Robert van den Brink <robertvdenbrink@gmail.com>
2017-12-29 17:19:16 +0100

    Boss

---
+++ DeGame/DeGame/GameObjects/Boss.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+
+public class Boss :  SpriteGameObject
+{
+    protected float health = 100;
+    protected float maxhealth = 100;
+    //protected float attack;
+    //protected float attackspeed;
+    //protected float range;
+    protected Vector2 basevelocity = new Vector2((float)0.5, (float)0.5);
+    public SpriteEffects Effects;
+    HealthBar healthbar;
+
+    public Boss(Vector2 startPosition, int layer = 0, string id = "Boss") : base("Sprites/Boss", layer, id)
+    {
+        position = startPosition;
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+    }
+
+    public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+    {
+        base.Draw(gameTime, spriteBatch);
+        spriteBatch.Draw(GameEnvironment.assetManager.GetSprite("Sprites/Boss"), position);
+    }
+}
