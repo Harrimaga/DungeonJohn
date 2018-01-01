aefc9ddb26bdf287ddcf5bff4ed65c76a60c69d5
Tim <timvanbrederode@live.nl>
2018-01-01 16:41:31 +0100

    Foreach weg

--- DeGame/DeGame/GameManagement/GameObjectList.cs
+++ DeGame/DeGame/GameManagement/GameObjectList.cs
@@ -32,7 +32,6 @@ public class GameObjectList : GameObject
         children.Add(obj);
     }
 
-    // Moeten misschien kijken of dit werkt
     public void Remove(GameObject obj)
     {
         children.Remove(obj);
