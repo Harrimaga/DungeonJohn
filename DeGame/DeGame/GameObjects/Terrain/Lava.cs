6526af9824afd162b65de9087a46061b2db46b44
Romeo Zeph <r.s.b.zeph@students.uu.nl>
2018-01-02 15:42:18 +0100

    Shop

---
+++ DeGame/DeGame/GameObjects/Terrain/Lava.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Lava : SpriteGameObject
+ {
+    public Lava(Vector2 startPosition, int layer = 0, string id = "Rock")
+    : base("Sprites/Rock Sprite", layer, id)
+    {
+
+    }
+ }
