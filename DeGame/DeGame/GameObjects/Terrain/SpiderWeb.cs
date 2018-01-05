20bb42dff86fb175a4ee25754cecd7a3bcd8ade3
Romeo Zeph <r.s.b.zeph@students.uu.nl>
2018-01-05 17:57:49 +0100

    SpiderWeb toegevoegd werkt alleen heeft het zelfde probleem als ice

---
+++ DeGame/DeGame/GameObjects/Terrain/SpiderWeb.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class SpiderWeb : Tiles
+{
+    public SpiderWeb(Vector2 startPosition, int layer = 0, string id = "SpiderWeb")
+    : base(startPosition, layer, id)
+    {
+        position = startPosition;
+    }
+    public override void Update(GameTime gameTime)
+    {
+        if (CollidesWith(PlayingState.player))
+        {
+            PlayingState.player.velocity = new Vector2(2.5f,2.5f);
+        }
+        else
+        {
+            PlayingState.player.velocity = PlayingState.player.velocitybase;
+        }
+    }
+
+    public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(GameEnvironment.assetManager.GetSprite("Sprites/Background Sprite"), position, Color.Gray);
+        spriteBatch.Draw(GameEnvironment.assetManager.GetSprite("Sprites/SpiderWeb"), position);
+    }
+}
