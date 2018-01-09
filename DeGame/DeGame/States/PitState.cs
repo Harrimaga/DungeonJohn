19db42c7e1d7014c0dd00dca8efd2fb84fda7be0
Fabian <fabia@DESKTOP-7MVPKR9>
2018-01-09 18:40:18 +0100

    Pit class werkt nu

--- DeGame/DeGame/States/PitState.cs
+++ DeGame/DeGame/States/PitState.cs
@@ -11,22 +11,30 @@ using System.Threading.Tasks;
 class PitState : IGameObject 
 {
     Random r;
-    Room pitroom;
     Player player2;
+    Room actualroom;
+    protected IGameObject playingState;
     int pitroomnumber;
+    int x = (int)PlayingState.currentFloor.currentRoom.position.X;
+    int y = (int)PlayingState.currentFloor.currentRoom.position.Y;
 
     public PitState()
     {
+        playingState = GameEnvironment.gameStateManager.GetGameState("Playing");
+
         r = new Random();
         int random = r.Next(100);
         if (random < 20)
             pitroomnumber = 111;
-        else if (random < 40)
-            pitroomnumber = 222;
+        else if (random < 50)
+            pitroomnumber = 111;
         else
-            pitroomnumber = 333;
+            pitroomnumber = 111;
 
-        pitroom = new Room(pitroomnumber, (int)PlayingState.currentFloor.currentRoom.position.X, (int)PlayingState.currentFloor.currentRoom.position.Y);
+        actualroom = PlayingState.currentFloor.floor[x, y];
+        PlayingState.currentFloor.floor[x, y] = null;
+        PlayingState.currentFloor.floor[x, y] = new Room(pitroomnumber, x, y);
+        PlayingState.currentFloor.floor[x, y].LoadTiles();
         player2 = PlayingState.player;
     }
 
@@ -35,21 +43,29 @@ class PitState : IGameObject
         if(inputHelper.KeyPressed(Keys.H))
         {
             GameEnvironment.gameStateManager.SwitchTo("Playing");
+            PlayingState.currentFloor.floor[x, y] = null;
+            PlayingState.currentFloor.floor[x, y] = new Room(actualroom.RoomListIndex, x, y);
+            PlayingState.currentFloor.DoorCheck();
+            for (int a = 0; a < 9; a++)
+                for (int b = 0; b < 9; b++)
+                    if(PlayingState.currentFloor.floor[a, b] != null)
+                        PlayingState.currentFloor.floor[a, b].LoadTiles(); 
+            actualroom = null;
             PlayingState.player = player2;
         }
+        PlayingState.currentFloor.floor[x, y].HandleInput(inputHelper);
         player2.HandleInput(inputHelper);
     }
 
     public virtual void Update(GameTime gameTime)
     {
-        pitroom.Update(gameTime);
+        PlayingState.currentFloor.floor[x, y].Update(gameTime);
         player2.Update(gameTime);
     }
 
     public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch)
     {
-        pitroom.LoadTiles();
-        pitroom.Draw(gameTime, spriteBatch);
+        PlayingState.currentFloor.floor[x, y].Draw(gameTime, spriteBatch);
         player2.Draw(gameTime, spriteBatch);
     }
 
