e5261e05f14eef9b56dcb00e7580f7b0632b9f65
Tim <timvanbrederode@live.nl>
2018-01-14 00:08:04 +0100

    Added tips for implementing the crafting system

--- DeGame/DeGame/Inventory/CraftingSlots.cs
+++ DeGame/DeGame/Inventory/CraftingSlots.cs
@@ -11,7 +11,6 @@ public class CraftingSlots : GameObjectList
     CraftingSlot itemSlot1, itemSlot2;
     new Vector2 position;
 
-
     public CraftingSlots(Vector2 position) : base()
     {
         this.position = position;
@@ -34,5 +33,30 @@ public class CraftingSlots : GameObjectList
             itemSlot1.position = position;
             itemSlot2.position = position + new Vector2(212, 0);
         }
+    }
+
+    public override void HandleInput(InputHelper inputHelper)
+    {
+        // TODO: Implement
+        // Kijk of er twee items in zitten
+        // Als er 2 in zitten en je klikt op de +, kijk of ze samen een ander item vormen
+        // Als dit zo is, verwijder de items in de slots (gewoon het item weghalen en niet in Player.inventory toevoegen)
+        //      en voeg het gecrafte item toe aan Player.inventory
+
+        // Deze handle input moet alleen kijken naar de + (of whatever je ervan maakt), dus niet naar de twee slots.
+        //      De craftingslots hebben zelf hun HandleInput (in CraftingSlot.cs), dus houdt hier rekening mee dat je de goede boundingbox gebruikt! :p
+    }
+
+    public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+    {
+        // TODO: draw functie implementeren.
+        // Hier wss de twee slots tekenen (met evt een + ertussen ofzo, verzin wat)
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        // TODO: imlement
+        // Weet niet of hier veel mee gedaan moet worden, misschien alleen de positie updaten ofzo? 
+        //      Hoeft niet als je in Crafting state elke keer in de draw of update een nieuwe instance maakt hiervan
     }
 }
