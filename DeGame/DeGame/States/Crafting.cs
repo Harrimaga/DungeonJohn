0f2d78b56ed5ed2c646bd33b84034a2f1c13559f
Romeo Zeph <r.s.b.zeph@students.uu.nl>
2018-01-13 13:36:51 +0100

    crafting begin

--- DeGame/DeGame/States/Crafting.cs
+++ DeGame/DeGame/States/Crafting.cs
@@ -9,7 +9,8 @@ using System.Threading.Tasks;
 
 class Crafting : IGameObject
 {
-    
+    List<InventorySlot> inventory;
+    Vector2 BasisPosition;
     public Crafting()
     {
     }
@@ -23,9 +24,29 @@ class Crafting : IGameObject
     public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch)
     {
         GameEnvironment.gameStateManager.GetGameState("Playing").Draw(gameTime, spriteBatch);
+
+
+        inventory = new List<InventorySlot>();
+        for (int i = 0; i < Player.inventory.items.Count; i++)
+        {
+            Vector2 slotPosition;
+            int x, y;
+            y = (int)Math.Floor((double)i / 9);
+            x = i % 9;
+            //slotPosition = wornItems.position + new Vector2(0, 200) + new Vector2(x * 74, y * 74);
+            slotPosition = BasisPosition + new Vector2(500 + x * 74, 450 + y * 74);
+
+            inventory.Add(new InventorySlot(slotPosition, Player.inventory.items[i]));
+        }
+
+        foreach (InventorySlot slot in inventory)
+        {
+            slot.Draw(gameTime, spriteBatch);
+        }
     }
     public virtual void Update(GameTime gameTime)
     {
+        BasisPosition = new Vector2(Camera.Position.X - (GameEnvironment.WindowSize.X / 2), Camera.Position.Y - (GameEnvironment.WindowSize.Y / 2));
     }
     public virtual void Reset()
     {
