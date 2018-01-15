3bbc41d969cecf7b677b23a7105bd9e13ad890e7
Fabian <fabia@DESKTOP-7MVPKR9>
2018-01-15 12:01:40 +0100

    pitstate

--- DeGame/DeGame/States/PitState.cs
+++ DeGame/DeGame/States/PitState.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -14,7 +15,6 @@ class PitState : IGameObject
     Player player2;
     Room actualroom;
     Vector2 exitposition, MiddleofPlayer;
-    protected IGameObject playingState;
     int pitroomnumber;
     int x = (int)PlayingState.currentFloor.currentRoom.position.X;
     int y = (int)PlayingState.currentFloor.currentRoom.position.Y;
@@ -28,9 +28,9 @@ class PitState : IGameObject
         if (random < 20)
             pitroomnumber = 111;
         else if (random < 50)
-            pitroomnumber = 222;
+            pitroomnumber = 111;
         else
-            pitroomnumber = 333;
+            pitroomnumber = 111;
 
         actualroom = PlayingState.currentFloor.floor[x, y];
         PlayingState.currentFloor.floor[x, y] = new Room(pitroomnumber, x, y);
@@ -49,37 +49,34 @@ class PitState : IGameObject
     {
         PlayingState.currentFloor.floor[x, y].Update(gameTime);
         player2.Update(gameTime);
+        PitExit(gameTime);
+    }
+
+    public void PitExit(GameTime gameTime)
+    {
         MiddleofPlayer = new Vector2(player2.position.X + GameEnvironment.assetManager.GetSprite("Sprites/PlayerFront").Width / 2, player2.position.Y + GameEnvironment.assetManager.GetSprite("Sprites/PlayerFront").Height / 2);
 
         if (PlayingState.currentFloor.floor[x, y].enemycounter == 0)
         {
-            if(MiddleofPlayer.X >= exitposition.X && MiddleofPlayer.X <= exitposition.X + PlayingState.currentFloor.currentRoom.CellWidth
+            if (MiddleofPlayer.X >= exitposition.X && MiddleofPlayer.X <= exitposition.X + PlayingState.currentFloor.currentRoom.CellWidth
                 && MiddleofPlayer.Y >= exitposition.Y + 120 && MiddleofPlayer.Y <= exitposition.Y + PlayingState.currentFloor.currentRoom.CellHeight + 120)
-                {
-                    GameEnvironment.gameStateManager.SwitchTo("Playing");
-                    PlayingState.currentFloor.floor[x, y] = new Room(actualroom.RoomListIndex, x, y);
-                    PlayingState.currentFloor.DoorCheck();
-
-                    for (int a = 0; a < 9; a++)
-                        for (int b = 0; b < 9; b++)
-                            if (PlayingState.currentFloor.floor[a, b] != null)
-                                PlayingState.currentFloor.floor[a, b].LoadTiles();
-
-                    PlayingState.player = player2;
-                PlayingState.player.position = actualroom.LastEntryPoint; //new Vector2(10 * PlayingState.currentFloor.currentRoom.CellWidth + x * PlayingState.currentFloor.currentRoom.roomwidth, 7 * PlayingState.currentFloor.currentRoom.CellHeight + y * PlayingState.currentFloor.currentRoom.roomheight);
-                }
+            {
+                PlayingState.currentFloor.floor[x, y] = actualroom;
+                PlayingState.currentFloor.DoorCheck();
+                PlayingState.player = player2;
+                PlayingState.player.position = actualroom.LastEntryPoint;
+                GameEnvironment.gameStateManager.SwitchTo("Playing");
+            }
         }
     }
 
     public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch)
     {
-        PlayingState.currentFloor.floor[x, y].Draw(gameTime, spriteBatch);
-
+        PlayingState.currentFloor.Draw(gameTime, spriteBatch);
         if (PlayingState.currentFloor.floor[x, y].enemycounter == 0)
         {
             spriteBatch.Draw((GameEnvironment.assetManager.GetSprite("Sprites/Tiles/StartTile")), exitposition);
         }
-
         player2.Draw(gameTime, spriteBatch);
     }
 
