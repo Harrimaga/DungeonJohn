961bdad7254684af9b9b8809b41d11ae0a353758
Tim <timvanbrederode@live.nl>
2018-01-19 16:42:40 +0100

    item drawing fix

--- DeGame/DeGame/GameObjects/Player.cs
+++ DeGame/DeGame/GameObjects/Player.cs
@@ -344,7 +344,27 @@ public class Player : SpriteGameObject
         {
             inventory.currentWeapon.DrawOnPlayer(gameTime, spriteBatch);
         }
-        
+        if (inventory.currentHelmet != null)
+        {
+            inventory.currentHelmet.DrawOnPlayer(gameTime, spriteBatch);
+        }
+        if (inventory.currentShield != null)
+        {
+            inventory.currentShield.DrawOnPlayer(gameTime, spriteBatch);
+        }
+        if (inventory.currentBoots != null)
+        {
+            inventory.currentBoots.DrawOnPlayer(gameTime, spriteBatch);
+        }
+        if (inventory.currentPassives[0] != null)
+        {
+            inventory.currentPassives[0].DrawOnPlayer(gameTime, spriteBatch);
+        }
+        if (inventory.currentPassives[1] != null)
+        {
+            inventory.currentPassives[1].DrawOnPlayer(gameTime, spriteBatch);
+        }
+
         spriteBatch.DrawString(GameEnvironment.assetManager.GetFont("Sprites/SpelFont"), "Player Level: " + Convert.ToString(level), new Vector2(PlayingState.currentFloor.screenwidth - 275 + (Camera.Position.X - PlayingState.currentFloor.screenwidth / 2), 200 + (Camera.Position.Y - PlayingState.currentFloor.screenheight / 2)), Color.White);
         spriteBatch.DrawString(GameEnvironment.assetManager.GetFont("Sprites/SpelFont"), "Damage: " + Convert.ToString(attack), new Vector2(PlayingState.currentFloor.screenwidth - 275 + (Camera.Position.X - PlayingState.currentFloor.screenwidth / 2), 225 + (Camera.Position.Y - PlayingState.currentFloor.screenheight / 2)), Color.Red);
         bullets.Draw(gameTime, spriteBatch);
