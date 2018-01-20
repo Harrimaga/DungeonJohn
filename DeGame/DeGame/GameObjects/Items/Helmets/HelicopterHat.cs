95fc6e53329167cf969c05f0420f87f065068560
Jeroen <Jeroen@HIJZ13>
2018-01-20 20:57:14 +0100

    helicopterhat geadd (doet nog niks) extraspeed is nu procentueel

---
+++ DeGame/DeGame/GameObjects/Items/Helmets/HelicopterHat.cs
@@ -0,0 +1,19 @@
+﻿class HelicopterHat : Item, IHelmet
+{
+    public HelicopterHat()
+    {
+        itemName = "HelicopterHat";
+        itemDescription = "Has anyone ever even worn one of these things? Grants the ability to fly";
+        Type = "helmet";
+    }
+    public override void equip()
+    {
+        //TODO: deze bool implementeren
+        //PlayingState.player.HelicopterHat = true;
+    }
+    public override void unequip()
+    {
+        //PlayingState.player.HelicopterHat = false;
+        base.unequip();
+    }
+}
