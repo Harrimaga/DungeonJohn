52ec40754b4e78421b82042d4b4f87f1229220f9
Tim <timvanbrederode@live.nl>
2018-01-23 12:30:12 +0100

    Documentation for the inventory

--- DeGame/DeGame/Inventory/Slots/WeaponSlot.cs
+++ DeGame/DeGame/Inventory/Slots/WeaponSlot.cs
@@ -12,7 +12,11 @@ public class WeaponSlot : Slot
     public WeaponSlot(Vector2 position, int layer = 0, string id = "WeaponSlot") : base ("Sprites/InventorySlots/WeaponSlot", layer, id)
     {
         this.position = position;
+
+        // Gets the sprite of the slot
         sprite = GameEnvironment.assetManager.GetSprite("Sprites/InventorySlots/WeaponSlot");
+
+        // If there's an item in the slot, it gets the sprite for that item
         if (Player.inventory.currentWeapon != null)
         {
             itemSprite = GameEnvironment.assetManager.GetSprite("Sprites/Items/" + Player.inventory.currentWeapon.itemName);
@@ -21,6 +25,7 @@ public class WeaponSlot : Slot
 
     public override void Update(GameTime gameTime)
     {
+        // Resets the itemsprite, in case the item was swapped
         if (Player.inventory.currentWeapon != null)
         {
             itemSprite = GameEnvironment.assetManager.GetSprite("Sprites/Items/" + Player.inventory.currentWeapon.itemName);
@@ -31,8 +36,13 @@ public class WeaponSlot : Slot
 
     public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
     {
+        // Draws the tooltips
         base.Draw(gameTime, spriteBatch);
+
+        // Draws the slot
         spriteBatch.Draw(sprite, position);
+
+        // If an item is in the slot, draw it with scaling based on the dimensions of the slot itself
         if (Player.inventory.currentWeapon != null)
         {
             InventorySlot.DrawItem(sprite, itemSprite, position, gameTime, spriteBatch);
