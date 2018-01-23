20596dd878c0d3dc186e4972b531f9bd3b461e86
Fabian <fabia@DESKTOP-7MVPKR9>
2018-01-23 20:58:08 +0100

    items meer rare dan andere

--- DeGame/DeGame/GameObjects/Items/ItemList.cs
+++ DeGame/DeGame/GameObjects/Items/ItemList.cs
@@ -18,34 +18,42 @@ class ItemList
 
     public void MakeItemList()
     {
-        RoomList.Add(new Mac10());
+        for(int x = 0; x < 2; x++)
+        {
+            RoomList.Add(new Mac10());
+            RoomList.Add(new MageJacket());
+            RoomList.Add(new SimpleArmour());
+            RoomList.Add(new Mirror());
+            RoomList.Add(new BloodRing());
+            RoomList.Add(new HardHelmet());
+            RoomList.Add(new CoolBoots());
+        }
+        
         RoomList.Add(new DoubleGun());
-        RoomList.Add(new SlimyBoots());
-        RoomList.Add(new CoolBoots());
-        RoomList.Add(new MageJacket());
-        RoomList.Add(new SimpleArmour());
-        RoomList.Add(new Mirror());
-        RoomList.Add(new CrestShield());
-        RoomList.Add(new BloodRing());
-        RoomList.Add(new VialOfPoison());
-        RoomList.Add(new HardHelmet());
+        RoomList.Add(new SlimyBoots());  
+        RoomList.Add(new CrestShield());       
+        RoomList.Add(new VialOfPoison());       
         RoomList.Add(new PowerHelmet());
         RoomList.Add(new HelicopterHat());
     }
 
     public void MakeShopList()
     {
-        ShopList.Add(new Mac10());
+        for (int x = 0; x < 2; x++)
+        {
+            ShopList.Add(new Mac10());
+            ShopList.Add(new MageJacket());
+            ShopList.Add(new SimpleArmour());
+            ShopList.Add(new Mirror());
+            ShopList.Add(new BloodRing());
+            ShopList.Add(new HardHelmet());
+            ShopList.Add(new CoolBoots());
+        }
+
         ShopList.Add(new DoubleGun());
         ShopList.Add(new SlimyBoots());
-        ShopList.Add(new CoolBoots());
-        ShopList.Add(new SimpleArmour());
-        ShopList.Add(new MageJacket());
-        ShopList.Add(new Mirror());
         ShopList.Add(new CrestShield());
-        ShopList.Add(new BloodRing());
         ShopList.Add(new VialOfPoison());
-        ShopList.Add(new HardHelmet());
         ShopList.Add(new PowerHelmet());
         ShopList.Add(new HelicopterHat());
     }
