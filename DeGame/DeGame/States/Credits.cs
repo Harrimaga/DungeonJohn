8737847afcba49f8ad594a0bd9886e3b1fe65d18
Robert van den Brink <robertvdenbrink@gmail.com>
2018-01-30 15:44:01 +0100

    credit roll

--- DeGame/DeGame/States/Credits.cs
+++ DeGame/DeGame/States/Credits.cs
@@ -4,8 +4,10 @@ using Microsoft.Xna.Framework.Input;
 
 class Credits : IGameObject
 {
-    Vector2 BasisPosition = new Vector2(Camera.Position.X - (GameEnvironment.WindowSize.X / 2), Camera.Position.Y - (GameEnvironment.WindowSize.Y / 2));
+    Vector2 position, Baseposition;
     Vector2 velocity = new Vector2(0, -0.1f);
+    int Counter = 0;
+    bool w = false;
     public Credits()
     {
     }
@@ -14,21 +16,29 @@ class Credits : IGameObject
         if (inputHelper.KeyPressed(Keys.Space))
             GameEnvironment.gameStateManager.SwitchTo("Victory");
     }
-    public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch)
-    {
-        spriteBatch.Draw(GameEnvironment.assetManager.GetSprite("Sprites/States/Credits"), BasisPosition);
-    }
+
     public virtual void Update(GameTime gameTime)
     {
-        //position += velocity * gameTime.ElapsedGameTime.Milliseconds;
-        //Counter++;
-        //if (Counter >= 1100)
-        //{
-        //    Reset();
-        //    GameEnvironment.gameStateManager.SwitchTo("MainMenu");
-        //}
-        
+        if (w == false)
+        {
+            position = new Vector2(Camera.Position.X - (GameEnvironment.WindowSize.X / 2), Camera.Position.Y - (GameEnvironment.WindowSize.Y / 2));
+            w = true;
+        }
+
+        Baseposition = position += velocity * gameTime.ElapsedGameTime.Milliseconds;
+        Counter += 1 * gameTime.ElapsedGameTime.Milliseconds;
+        if (Counter >= 55000)
+        {
+            //Reset();
+            GameEnvironment.gameStateManager.SwitchTo("MainMenu");
+        }
     }
+
+    public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(GameEnvironment.assetManager.GetSprite("Sprites/States/Credits"), Baseposition);
+    }
+
     public virtual void Reset()
     {
     }
